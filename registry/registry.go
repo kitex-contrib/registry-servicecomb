e73d635c46144795eaaceeb407ee98192413cfb9
songxi <sungxisx@icloud.com>
2022-07-17 21:50:51 +0800

    registry and UT

--- registry/registry.go
+++ registry/registry.go
@@ -6,10 +6,60 @@ import (
 	"github.com/cloudwego/kitex/pkg/registry"
 	"github.com/go-chassis/cari/discovery"
 	"github.com/go-chassis/sc-client"
+	"github.com/kitex-contrib/registry-servicecomb/servicecomb"
 )
 
+type options struct {
+	appId      string
+	version    string
+	hostName   string
+	serviceId  string
+	instanceId string
+}
+
+// Option is ServiceComb option.
+type Option func(o *options)
+
+func WithAppId(appId string) Option {
+	return func(o *options) {
+		o.appId = appId
+	}
+}
+
+func WithVersion(version string) Option {
+	return func(o *options) {
+		o.version = version
+	}
+}
+
+func WithHostName(hostName string) Option {
+	return func(o *options) {
+		o.hostName = hostName
+	}
+}
+
 type serviceCombRegistry struct {
-	cli sc.Client
+	cli  *sc.Client
+	opts options
+}
+
+func NewDefaultServiceCombRegistry(opts ...Option) (registry.Registry, error) {
+	client, err := servicecomb.NewDefaultServiceCombClient()
+	if err != nil {
+		return nil, err
+	}
+	return NewServiceCombRegistry(client, opts...), nil
+}
+
+func NewServiceCombRegistry(client *sc.Client, opts ...Option) registry.Registry {
+	op := options{
+		appId:   "DEFAULT",
+		version: "DEFAULT",
+	}
+	for _, opt := range opts {
+		opt(&op)
+	}
+	return &serviceCombRegistry{cli: client, opts: op}
 }
 
 func (scr *serviceCombRegistry) Register(info *registry.Info) error {
@@ -25,21 +75,39 @@ func (scr *serviceCombRegistry) Register(info *registry.Info) error {
 
 	serviceId, err := scr.cli.RegisterService(&discovery.MicroService{
 		ServiceName: info.ServiceName,
-		AppId:       info.ServiceName,
+		AppId:       scr.opts.appId,
+		Version:     scr.opts.version,
 		Status:      "UP",
 	})
 	if err != nil {
 		return fmt.Errorf("register service error: %w", err)
 	}
+	scr.opts.serviceId = serviceId
 
-	_, err = scr.cli.RegisterMicroServiceInstance(&discovery.MicroServiceInstance{
+	instanceId, err := scr.cli.RegisterMicroServiceInstance(&discovery.MicroServiceInstance{
 		ServiceId:  serviceId,
 		Endpoints:  []string{info.Addr.String()},
-		Status:     "UP",
+		HostName:   scr.opts.hostName,
+		Status:     sc.MSInstanceUP,
 		Properties: info.Tags,
 	})
 	if err != nil {
 		return fmt.Errorf("register service instance error: %w", err)
+	}
+	scr.opts.instanceId = instanceId
+
+	return nil
+}
+
+func (scr *serviceCombRegistry) Deregister(info *registry.Info) error {
+	_, err := scr.cli.UnregisterMicroServiceInstance(scr.opts.serviceId, scr.opts.instanceId)
+	if err != nil {
+		return fmt.Errorf("Deregister service instance error: %w", err)
+	}
+
+	_, err = scr.cli.UnregisterMicroService(scr.opts.serviceId)
+	if err != nil {
+		return fmt.Errorf("Deregister service error: %w", err)
 	}
 
 	return nil
---
+++ registry/registry_test.go
@@ -0,0 +1,113 @@
+package registry
+
+import (
+	"github.com/cloudwego/kitex/pkg/registry"
+	"github.com/go-chassis/sc-client"
+	"github.com/stretchr/testify/assert"
+	"net"
+	"testing"
+	"time"
+)
+
+func getServiceCombClient() (*sc.Client, error) {
+	client, err := sc.NewClient(sc.Options{
+		Endpoints: []string{"127.0.0.1:30100"},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func TestNewDefaultServiceCombRegistry(t *testing.T) {
+	client, err := getServiceCombClient()
+	if err != nil {
+		t.Errorf("err:%v", err)
+	}
+	got := NewServiceCombRegistry(client, WithAppId("DEFAULT"), WithVersion("DEFAULT_GROUP"))
+	assert.NotNil(t, got)
+}
+
+//  test registry a service
+func TestServiceCombRegistryRegister(t *testing.T) {
+	client, err := getServiceCombClient()
+	if err != nil {
+		t.Errorf("err:%v", err)
+		return
+	}
+	type fields struct {
+		cli *sc.Client
+	}
+	type args struct {
+		info *registry.Info
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name:   "common",
+			fields: fields{client},
+			args: args{info: &registry.Info{
+				ServiceName: "demo.kitex-contrib.local",
+				Addr:        &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 8080},
+				Weight:      999,
+				StartTime:   time.Now(),
+				Tags:        map[string]string{"env": "local"},
+			}},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := NewServiceCombRegistry(tt.fields.cli, WithAppId("DEFAULT"), WithVersion("0.1"), WithHostName("ServiceComb-Test"))
+			if err := n.Register(tt.args.info); (err != nil) != tt.wantErr {
+				t.Errorf("Register() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+// test deregister a service
+func TestServiceCombRegistryDeregister(t *testing.T) {
+	client, err := getServiceCombClient()
+	if err != nil {
+		t.Errorf("err:%v", err)
+		return
+	}
+	type fields struct {
+		cli *sc.Client
+	}
+	type args struct {
+		info *registry.Info
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "common",
+			args: args{info: &registry.Info{
+				ServiceName: "demo.kitex-contrib.local",
+				Addr:        &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 8080},
+				Weight:      999,
+				StartTime:   time.Now(),
+				Tags:        map[string]string{"env": "local"},
+			}},
+			fields:  fields{client},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := NewServiceCombRegistry(tt.fields.cli, WithAppId("DEFAULT"), WithVersion("0.1"), WithHostName("ServiceComb-Test"))
+			if err := n.Deregister(tt.args.info); (err != nil) != tt.wantErr {
+				t.Errorf("Deregister() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
