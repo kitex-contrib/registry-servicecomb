fadebbcae39a524e39c42440cd6a2dd9f943472f
songxi <sungxisx@icloud.com>
2022-07-17 21:51:30 +0800

    registry and UT

--- servicecomb/client.go
+++ servicecomb/client.go
@@ -2,7 +2,7 @@ package servicecomb
 
 import sc "github.com/go-chassis/sc-client"
 
-func NewDefaultClient() (*sc.Client, error) {
+func NewDefaultServiceCombClient() (*sc.Client, error) {
 	client, err := sc.NewClient(sc.Options{
 		Endpoints: []string{"127.0.0.1:30100"},
 	})
